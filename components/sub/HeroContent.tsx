2e158e8881b5b7d01da402c1b066c41ab4d37c2c
Parth Solanki <parthsolanki1498@gmail.com>
2024-04-26 02:41:41 -0400

    typewriter effect added

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,59 +1,81 @@
-"use client"
-import React from 'react'
-import {motion} from  'framer-motion'
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from '@/utils/motion'
-import SparklesIcon from '@heroicons/react/24/solid/SparklesIcon'
-import Image from 'next/image'
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "@/utils/motion";
+import SparklesIcon from "@heroicons/react/24/solid/SparklesIcon";
+import Image from "next/image";
+import Typewriter from "typewriter-effect";
+
 const HeroContent = () => {
   return (
     <motion.div
-    initial="hidden"
-    animate="visible"
-    className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
-        <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
-            <motion.div
-            variants={slideInFromTop}
-            className='Welcome-box py-[15px] px-[7px] border border-[#7042f88b] opacity-[0.9]'
-            >
-                <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-                <h1 className='Welcome-text text-[13px]'>Fullstack Developer Portfolio</h1>
-            </motion.div>
-
-            <motion.div 
-            variants={slideInFromLeft(0.5)}
-            className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'>
-                <span>
-                Providing
-                <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best </span>
-                project experience
-                </span>
-            </motion.div>
-
-            <motion.p
-            variants={slideInFromLeft(0.8)}
-            className='text-lg text-gray-400 my-5 max-w-[600px]'>
-                I&apos;m a fullstack developer with a passion for creating beautiful and functional websites. Check out my projects below!
-            </motion.p>
-            <motion.a
-            variants={slideInFromLeft(1)}
-            className='py-2 button-primary text-center text-white cursor-point rounded-lg max-w-[200px]'>
-                Learn More!
-            </motion.a>
-        </div>
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[15px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Fullstack Developer Portfolio
+          </h1>
+        </motion.div>
 
         <motion.div
-        variants={slideInFromRight(0.8)}
-        className='w-full h-full flex justify-center items-center'>
-            <Image
-            className='rounded-full justify-center items-center'
-            src="/profile_picture.jpg"
-            alt="work icons"
-            height={520}
-            width={520} />
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Hi I'm
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+              {" "}
+              Parth Solanki!{" "}
+            </span>
+            <Typewriter
+              options={{
+                strings: ["Backend", "Frontend", "Umm... Full-stack!"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
         </motion.div>
-    </motion.div>
-  )
-}
 
-export default HeroContent
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          From backend depths to frontend skies, I'm on a journey to bridge the gap and become a cosmic fullstack explorer.
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-point rounded-lg max-w-[200px]"
+        >
+          Download Resume!
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromRight(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <Image
+          className="rounded-full justify-center items-center"
+          src="/profile_picture.jpg"
+          alt="work icons"
+          height={520}
+          width={520}
+        />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default HeroContent;
